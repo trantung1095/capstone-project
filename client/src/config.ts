7366f069ca54b64e7df1df3287937595e7638379
trantung <trantung1095@gmail.com>
2023-03-23 00:15:52 +0700

    update config client

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,12 +1,12 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'j8lnezvix9'
+const apiId = 'nwjzzy7798'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
   domain: 'dev-0zlqjaggqu314b68.us.auth0.com', // Auth0 domain
-  clientId: 'Lf4JkLncI9dsdwHoAwJvL8gHzBj7JgFb', // Auth0 client id
+  clientId: '4DRem3OtLrSpEZeOCWGAorJbVY4DU9Vl', // Auth0 client id
   callbackUrl:
     'http://trantung-todo-webapp.s3-website-us-east-1.amazonaws.com/callback'
 }
