e5978cf6c2985d152c5031d5f136bd5bc44ca136
trantung <trantung1095@gmail.com>
2023-03-30 22:59:18 +0700

    change upload image function

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -2,65 +2,54 @@ import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
-import { cors, httpErrorHandler } from 'middy/middlewares'
+import { cors } from 'middy/middlewares'
 
 import { getTodosForUser as getTodosForUser } from '../../helpers/todos'
-import {
-  getUserId,
-  parseLimitParameter,
-  parseNextKeyParameter,
-  parseOrderByParameter
-} from '../utils'
-import { GetTodosResponse } from '../../models/GetTodoResponse'
+import { getUserId, parseLimitParameter, parseNextKeyParameter } from '../utils'
+import { createLogger } from '../../utils/logger'
+const logger = createLogger('getTodos')
 
 // TODO: Get all TODO items for a current user
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const userId: string = getUserId(event)
-    let nextKey
-    let limit
-    let orderBy
+    // Pagination
+    const limit = parseLimitParameter(event) || 9
+    const key = parseNextKeyParameter(event)
 
-    try {
-      // Parse query parameters
-      nextKey = parseNextKeyParameter(event)
-      limit = parseLimitParameter(event) || 10
-      orderBy = parseOrderByParameter(event) || ''
-    } catch (e) {
-      console.log('Failed to parse query parameters: ', e.message)
-      return {
-        statusCode: 400,
-        headers: {
-          'Access-Control-Allow-Origin': '*'
-        },
-        body: JSON.stringify({
-          error: 'Invalid parameters'
-        })
+    // Get todos
+    logger.info('get Todos For User')
+    const userId = getUserId(event)
+    const result = await getTodosForUser(userId, limit, key)
+
+    // Check if this is last page, set nextKey empty
+    if (result.nextKey !== null) {
+      const nextKeyDecode = decodeURIComponent(result.nextKey)
+      const nextKey = JSON.parse(nextKeyDecode)
+
+      logger.info('Check next page')
+      const checkLastPage = await getTodosForUser(userId, 1, nextKey)
+      if (checkLastPage.items === null || checkLastPage.items.length === 0) {
+        logger.info('This is last page')
+        result.nextKey = null
       }
     }
 
-    const response: GetTodosResponse = await getTodosForUser(
-      userId,
-      nextKey,
-      limit,
-      orderBy
-    )
-
-    return {
+    const response = {
       statusCode: 200,
       headers: {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials': true
       },
       body: JSON.stringify({
-        items: response.items,
-        nextKey: response.nextKey
+        items: result.items,
+        nextKey: result.nextKey
       })
     }
+    return response
   }
 )
 
-handler.use(httpErrorHandler()).use(
+handler.use(
   cors({
     credentials: true
   })
